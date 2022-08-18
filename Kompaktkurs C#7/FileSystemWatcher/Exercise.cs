9e6c031d6d63f5bfc7822cc6b098b1827528ac02
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-08-18 15:30:05 +0200

    Add files via upload

---
+++ Kompaktkurs C#7/FileSystemWatcher/Exercise.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ex = new Exercise();
+            var path = @"C:\temp";
+            ex.Init(path);
+
+            Console.WriteLine("Directory:" + path);
+            Console.WriteLine("'q' to quit");
+            while (Console.Read() != 'q') ;
+        }
+    }
+}
+    public class Exercise
+    {
+        private readonly FileSystemWatcher Monitor = new FileSystemWatcher();
+
+        public void Init(string path)
+        {
+            
+            Monitor.Path = path;
+
+            Monitor.NotifyFilter = NotifyFilters.DirectoryName;
+
+            Monitor.Changed += OnChanged;
+            Monitor.Created += OnChanged;
+            Monitor.Deleted += OnChanged;
+            Monitor.Disposed +=OnDisposed;
+            Monitor.Renamed += OnRenamed;
+
+            Monitor.Filter = "*.*";
+
+            Monitor.EnableRaisingEvents = true;
+            Monitor.IncludeSubdirectories = true;
+
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine(string.Format("{0} has been renamed to {1}", e.OldName, e.Name));
+        }
+
+        private static void OnDisposed(object sender, EventArgs e)
+        {
+            Console.WriteLine(string.Format("{0}", e.ToString()));
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(string.Format("File : {0}, {1}", e.FullPath, e.ChangeType));
+        }
+}
+//|NotifyFilters.Size
---
+++ Kompaktkurs C#7/FileSystemWatcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var directory = Directory.GetCurrentDirectory();
+            var directory = @"C:\temp";
+            var Watcher = new Watcher();
+            Watcher.Init(directory);
+
+            Console.WriteLine("Zeichnis:" + directory);
+            Console.WriteLine("'q' to quit");
+            while (Console.Read() != 'q') ;
+        }
+    }
+}
+
+public class Watcher
+{
+    private readonly FileSystemWatcher m_watcher = new FileSystemWatcher();
+    public void Init(string path)
+    {
+        m_watcher.Path = path;
+
+        m_watcher.NotifyFilter = NotifyFilters.Size|NotifyFilters.DirectoryName|NotifyFilters.FileName;
+        
+        m_watcher.Filter = "*.*";
+
+        m_watcher.Changed += OnChanged;
+        m_watcher.Created += OnChanged;
+        m_watcher.Deleted += OnChanged;
+        m_watcher.Renamed += OnRenamed;
+
+        m_watcher.EnableRaisingEvents = true;
+        m_watcher.IncludeSubdirectories = true;
+    }
+
+    private static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        Console.WriteLine($"File: {e.FullPath} {e.ChangeType} ");
+    }
+    private static void OnRenamed(object source, RenamedEventArgs e)
+    {
+        Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
+    }
+}
+
+/* 
+| NotifyFilters.LastAccess
+| NotifyFilters.LastWrite
+| NotifyFilters.FileName
+| NotifyFilters.DirectoryName
+*/
